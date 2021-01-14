28bfdf243241477ccf59356c87dbcf5c336e087a
Chelsey Dewolf <dewolfchelsey@gmail.com>
2021-01-14 16:28:56 +0100

    Merge

---
+++ .nuxt/router.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from '@nuxt/ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _8c3d1ac0 = () => interopDefault(import('../pages/login.vue' /* webpackChunkName: "pages/login" */))
+const _d2d7bfee = () => interopDefault(import('../pages/secret.vue' /* webpackChunkName: "pages/secret" */))
+const _1f8c60ee = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/login",
+    component: _8c3d1ac0,
+    name: "login"
+  }, {
+    path: "/secret",
+    component: _d2d7bfee,
+    name: "secret"
+  }, {
+    path: "/",
+    component: _1f8c60ee,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+function decodeObj(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === 'string') {
+      obj[key] = decode(obj[key])
+    }
+  }
+}
+
+export function createRouter () {
+  const router = new Router(routerOptions)
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    const r = resolve(to, current, append)
+    if (r && r.resolved && r.resolved.query) {
+      decodeObj(r.resolved.query)
+    }
+    return r
+  }
+
+  return router
+}
